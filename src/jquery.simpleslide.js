fe2b6385e41744ef515eabe5fc7591f1d91f0c90
Nicolas Zhao <nicolas-zhao@hotmail.com>
2014-02-14 17:04:32 +0800

    add test unit

--- src/jquery.simpleslide.js
+++ src/jquery.simpleslide.js
@@ -9,18 +9,37 @@
 (function($) {
 
 	$.fn.simpleslide = function(options) {
-		options = $.extend({}, $.fn.simpleslide.defaults, options);
-
-		return this.each(function() {
-			new $.fn.simpleslide.Slide(this, options);
-		});
+		var args = Array.prototype.slice.call(arguments, 1);
+		
+		if (typeof options === 'string') {
+			this.each(function() {
+				var inst = $(this).data('simpleslide');
+				
+				if (inst && typeof inst[options] === 'function' && options.charAt(0) !== '_') {
+					inst[options].apply(inst, args);
+				}
+			});
+		} else {
+			options = $.extend({}, $.fn.simpleslide.defaults, options);
+			this.each(function() {
+				var inst = $(this).data('simpleslide');
+				
+				if (inst) {
+					inst.option(options);
+				} else {
+					$(this).data('simpleslide', $.fn.simpleslide.Slide(this, options));
+				}
+			});
+		}
+		
+		return this;
 	};
 	
 	$.fn.simpleslide.defaults = {
 		visible: 3,
 		scroll: 1,
 		duration: 800,
-		auto: true,
+		auto: false,
 		circular: true,
 		vertical: false,
 		prev: null,
@@ -36,21 +55,20 @@
 	};
 	
 	$.fn.simpleslide.Slide = function(target, options) {
-		this._$target = $(target);
-		this._options = this._adjustOptions(options);
-		this._extra = Math.ceil(this._options.extra);
-		this._direction = this._options.vertical ? 'top' : 'left';
+		if (!(this instanceof $.fn.simpleslide.Slide)) {
+			return new $.fn.simpleslide.Slide(target, options);
+		}
+		
+		this._$target = $(target).addClass('simpleslide');
+		this._options = options;
+		this._adjustOptions();
+		
+		this._extra = 0;
+		this._direction = 'left';
 		this._current = 0;
 		this._forward = false;
 		this._timer = null;
 		
-		this._$container = this._$target.wrapInner('<div class="simpleslide" />').find('.simpleslide');
-		this._$slide = this._$container.wrapInner('<div class="simpleslide-wrapper">').find('.simpleslide-wrapper');
-		this._$items = this._getItems();
-		this._itemWidth = this._$items.outerWidth(true);
-		this._itemHeight = this._$items.outerHeight(true);
-		this._length = this._$items.length;		
-
 		this._create();
 	};
 
@@ -58,58 +76,21 @@
 		constructor: $.fn.simpleslide.Slide,
 		
 		_create: function() {
-			var propWidth = this._options.vertical ? 'height' : 'width',
-				propHeight = this._options.vertical ? 'width' : 'height',
-				that = this;
+			var that = this;
 			
-			this._current = this._options.start + (this._options.circular ? this._options.visible + this._extra : 0);
-			if (!this._options.circular && (this._current + this._options.visible > this._length)) {
-				this._current = this._length - this._options.visible;
-			}
+			this._$target
+				.data('simpleslide-original-html', this._$target.html())
+				.on('mouseenter.simpleslide', $.proxy(this.stop, this))
+				.on('mouseleave.simpleslide', $.proxy(this.repeat, this))
+				.on('click.simpleslide', '.simpleslide-prev', $.proxy(this.prev, this))
+				.on('click.simpleslide', '.simpleslide-next', $.proxy(this.next, this)),
 			
-			this._extraWidth = Math.floor((this._options.vertical ? this._itemHeight : this._itemWidth) * this._options.extra);
-			
-			this._$items.css({
-				width: this._$items.width(),
-				height: this._$items.height(),
-				overflow: 'hidden',
-				'float': 'left'
-			});
-
-			this._$slide
-				.css({position: 'absolute', 'z-index': 1})
-				.css(propWidth, this._calcSize(this._length, false))
-				.css(this._direction, -this._calcSize(this._current));
-
-			this._$container
-				.css({position: 'relative', 'z-index': 2, overflow: 'hidden'})
-				.css(propWidth, this._calcSize(this._options.visible, false) + (this._options.circular ? 
-								2 : 
-								(this._length > this._options.visible ? 1 : 0)) * 
-								this._extraWidth)
-								
-				.css(propHeight, this._options.vertical ? this._itemWidth : this._itemHeight);
-
-			this._$target.css({
-				width: this._options.vertical ? this._itemWidth : this._$container.width(),
-				position: 'relative'
-			}).on('mouseenter.simpleslide', function() {
-				that.stop();
-			}).bind('mouseleave.simpleslide', function() {
-				that.repeat();
-			});
-			
-			if (this._options.showButtons) {
-				this._generateButtons();
-			}
-			
-			if (this._options.auto) {
-				this.repeat();
-			}
+			this._refresh();
 		},
 		
-		_adjustOptions: function(options) {
-			var len = this._$target.children().length;
+		_adjustOptions: function() {
+			var len = this._$target.children().length,
+				options = this._options;
 				
 			if (len < options.visible) {
 				options.visible = len;
@@ -123,11 +104,13 @@
 				options.extra = 0;
 			}
 			
-			return options;
+			if (options.start > len - 1) {
+				options.start = 0;
+			}
 		},
 		
 		_getItems: function() {
-			var $items = this._$slide.children(), 
+			var $items = this._$slide.children().addClass('simpleslide-item'), 
 				visible = this._options.visible,
 				len = $items.length,
 				extra = (len > visible ? this._extra : 0);
@@ -165,16 +148,7 @@
 				that = this, 
 				$target = this._$target.prepend('<a class="simpleslide-button simpleslide-prev" href="">Prev</a>' + 
 					'<a class="simpleslide-button simpleslide-next" href="">Next</a>')
-					.find('.simpleslide-button').css({position: 'absolute', 'z-index': 3})
-					.end()
-					.on('click.simpleslide', '.simpleslide-prev', function(event) {
-						that.prev();
-						event.preventDefault();
-					})
-					.on('click.simpleslide', '.simpleslide-next', function(event) {
-						that.next();
-						event.preventDefault();
-					}),
+					.find('.simpleslide-button').css({position: 'absolute', 'z-index': 3}).end(),
 					
 				$prev = $target.find('.simpleslide-prev'), $next = $target.find('.simpleslide-next'), 
 				prevWidth = $prev.outerWidth(), prevHeight = $prev.outerHeight(), 
@@ -198,15 +172,7 @@
 				visible = this._options.visible, 
 				scroll = this._options.scroll, 
 				callback = this._options.beforeSlide, 
-				index, size,
-				
-				generateData = function() {
-					var data = {};
-					return this._$items.slice(this._current).slice(0, visible);
-					return {
-						items: this._$items.slice(this._current).slice(0, visible)
-					};
-				};
+				index, size;
 			
 			this._$slide.finish();
 			
@@ -258,14 +224,93 @@
 			this._current = to;
 		},
 		
+		_refresh: function() {
+			var propWidth = this._options.vertical ? 'height' : 'width',
+				propHeight = this._options.vertical ? 'width' : 'height';
+				
+			this._extra = Math.ceil(this._options.extra);
+			this._direction = this._options.vertical ? 'top' : 'left';
+			this._current = 0;
+			this._forward = false;
+			
+			this._$container = this._$target.wrapInner('<div class="simpleslide-container" />').find('.simpleslide-container');
+			this._$slide = this._$container.wrapInner('<div class="simpleslide-items">').find('.simpleslide-items');
+			this._$items = this._getItems();
+			this._itemWidth = this._$items.outerWidth(true);
+			this._itemHeight = this._$items.outerHeight(true);
+			this._length = this._$items.length;	
+			
+			this._current = this._options.start + (this._options.circular ? this._options.visible + this._extra : 0);
+			if (!this._options.circular && (this._current + this._options.visible > this._length)) {
+				this._current = this._length - this._options.visible;
+			}
+			
+			this._extraWidth = Math.floor((this._options.vertical ? this._itemHeight : this._itemWidth) * this._options.extra);
+			
+			this._$items.css({
+				width: this._$items.width(),
+				height: this._$items.height(),
+				overflow: 'hidden',
+				'float': 'left'
+			});
+
+			this._$slide
+				.css({position: 'absolute', 'z-index': 1})
+				.css(propWidth, this._calcSize(this._length, false))
+				.css(this._direction, -this._calcSize(this._current));
+
+			this._$container
+				.css({position: 'relative', 'z-index': 2, overflow: 'hidden'})
+				.css(propWidth, this._calcSize(this._options.visible, false) + (this._options.circular ? 
+								2 : 
+								(this._length > this._options.visible ? 1 : 0)) * 
+								this._extraWidth)
+								
+				.css(propHeight, this._options.vertical ? this._itemWidth : this._itemHeight);
+
+			this._$target.css({
+				width: this._options.vertical ? this._itemWidth : this._$container.width(),
+				position: 'relative'
+			});
+			
+			if (this._options.prev) {
+				$(this._options.prev).on('click.simpleslide', $.proxy(this.prev, this));
+			}
+			
+			if (this._options.next) {
+				$(this._options.next).on('click.simpleslide', $.proxy(this.next, this));
+			}
+			
+			if (this._options.showButtons) {
+				this._generateButtons();
+			}
+			
+			if (this._options.auto) {
+				this.repeat();
+			}
+		},
+		
+		option: function(options) {
+			this._$slide.finish();
+			this.stop();
+			
+			this._$target.html(this._$target.data('simpleslideOriginalHtml'));
+			$.extend(this._options, options);
+			
+			this._adjustOptions();
+			this._refresh();
+		},
+		
 		next: function() {
 			this._forward = true;
 			this._run(this._current + this._options.scroll);
+			return false;
 		},
 		
 		prev: function() {
 			this._forward = false;
 			this._run(this._current - this._options.scroll);
+			return false;
 		},
 		
 		repeat: function(speed) {
@@ -273,14 +318,14 @@
 				return;
 			}
 			
-			var that = this,
-				defaultSpeed = 2000;
+			var interval = (this._options.auto === true ? 2000 : this._options.auto),
+				that = this;
 			
-			speed = speed || defaultSpeed;
+			speed = speed || interval;
 			
 			this._timer = setTimeout(function() {
 				that.next();
-				that.repeat((that._options.auto === true ? defaultSpeed : that._options.auto) + that._options.duration);
+				that.repeat(interval + that._options.duration);
 			}, speed);
 		},
 		
--- test/simpleslide-test.js
+++ test/simpleslide-test.js
@@ -1,62 +1,105 @@
 (function($) {
-  /*
-    ======== A Handy Little QUnit Reference ========
-    http://api.qunitjs.com/
-
-    Test methods:
-      module(name, {[setup][ ,teardown]})
-      test(name, callback)
-      expect(numberOfAssertions)
-      stop(increment)
-      start(decrement)
-    Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      throws(block, [expected], [message])
-  */
-
-  module('jQuery#simpleslide', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is chainable', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.simpleslide(), this.elems, 'should be chainable');
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.simpleslide().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.simpleslide');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.simpleslide(), 'awesome.', 'should be awesome');
-    strictEqual($.simpleslide({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':simpleslide selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':simpleslide').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });
-
+	
+	var getVisibleItemsText = function(items) {
+		return items.map(function() {
+			return $.trim($(this).text());
+		}).get().join(',');
+	};
+	
+	module('simpleslide: options');
+	
+	test('visible', function() {
+		var $slide = $('#simpleslide').simpleslide({extra: 0});
+			itemWidth = $slide.find('.simpleslide-item').outerWidth(true);
+		
+		equal($slide.width(), itemWidth * 3, 'the default display three items');
+		
+		$slide.simpleslide('option', {
+			visible: 5
+		});
+		
+		// 4 is the original length of items
+		equal($slide.width(), itemWidth * 4, 'When visible is greater than the slide number, visible number will set to the slide number');
+	});
+	
+	test('scroll', function() {
+		var $slide = $('#simpleslide').simpleslide({
+				start: 4,
+				beforeSlide: function(data) {
+					var ret = getVisibleItemsText(data.newItems);
+					equal(ret, '2,3,4');
+				}
+			});
+			
+		$slide.simpleslide('next');
+	});
+	
+	asyncTest('auto: true', function() {
+		var $items = null,
+			$slide = $('#simpleslide').simpleslide({
+				auto: true,
+				beforeSlide: function(slide) {
+					$items = slide.newItems; 
+				}
+			});
+		
+		setTimeout(function() {
+			var ret = getVisibleItemsText($items);
+			
+			equal(ret, '2,3,4', 'default auto: 2000');
+			start();
+		}, 2000);
+	});
+	
+	asyncTest('auto: 1000', function() {
+		var $items = null,
+			$slide = $('#simpleslide').simpleslide({
+				auto: 1000,
+				beforeSlide: function(slide) {
+					$items = slide.newItems; 
+				}
+			});
+			
+		setTimeout(function() {
+			var ret = getVisibleItemsText($items);
+			
+			equal(ret, '2,3,4', 'auto: 1000');
+			start();
+		}, 1000);
+	});
+	
+	test('circular', function() {
+		var $items, 
+			$slide = $('#simpleslide').simpleslide({
+				beforeSlide: function(slide) {
+					$items = slide.newItems;
+				}
+			});
+		
+		$slide.simpleslide('prev');
+		equal(getVisibleItemsText($items), '4,1,2', 'backward');
+	});
+	
+	test('start', function() {
+		var $items, 
+			$slide = $('#simpleslide').simpleslide({
+				start: 5,
+				beforeSlide: function(slide) {
+					$items = slide.oldItems;
+				}
+			});
+		
+		$slide.simpleslide('next');
+		equal(getVisibleItemsText($items), '1,2,3', 'when start is greater than the last item index, start will set to the first item index');
+	});
+	
+	test('extra', function() {
+		var $slide = $('#simpleslide').simpleslide({
+				extra: 0.7
+			}),
+			itemWidth = $slide.find('.simpleslide-item').outerWidth(true);
+		
+		equal($slide.width(), Math.floor(itemWidth * 0.7) * 2 + itemWidth * 3);
+	});
+	
 }(jQuery));
